ae3ad890b63d662484404cb47fdf289a409add46
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 22:53:50 +0530

    added code now it throws exception when there is negative number

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -4,14 +4,31 @@ import java.util.Arrays;
 
 public class StringCalc {
 
-    public static int add(String s) {
+    public static int add(String s) throws NegativeNumberException{
         if(!s.isEmpty()){
             String delim = "[,\n]";
             if(s.length() > 2 && s.substring(0,2).equals("//")){
                 delim = s.split("\n")[0].substring(2);
                 s = s.substring(s.indexOf("\n")+1);
             }
-            return Arrays.stream(s.split(delim)).map(x->Integer.parseInt(x)).reduce(0, (sum, x)-> sum+x);
+            String nums[] = s.split(delim);
+            int negNumbers[] = new int[nums.length];
+            int i = 0, sum = 0;
+            for(String num: nums){
+                int n = Integer.parseInt(num);
+                if(n < 0){
+                    negNumbers[i++] = n;
+                }
+                sum += n;
+            }
+            if(i > 0){
+                StringBuilder msg = new StringBuilder();
+                while(i > 0){
+                    msg.append(negNumbers[--i] + " ");
+                }
+                throw new NegativeNumberException(msg.toString());
+            }
+            return sum;
         }
         return 0;
     }
