014b27396c771f9dd8895a7903f31cc718452fa0
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 21:53:16 +0530

    test fixed for single number

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -3,6 +3,8 @@ package src.main.java;
 public class StringCalc {
 
     public static int add(String s) {
+        if(!s.isEmpty())
+            return Integer.parseInt(s);
         return 0;
     }
 }
