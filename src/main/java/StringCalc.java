f86a32bbfbbab9917a172143cfac259ccef33acf
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 22:06:43 +0530

    fixed code to handle , and new line separator

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -6,7 +6,7 @@ public class StringCalc {
 
     public static int add(String s) {
         if(!s.isEmpty()){
-            return Arrays.stream(s.split(",")).map(x->Integer.parseInt(x)).reduce(0, (sum, x)-> sum+x);
+            return Arrays.stream(s.split("[,\n]")).map(x->Integer.parseInt(x)).reduce(0, (sum, x)-> sum+x);
         }
         return 0;
     }
