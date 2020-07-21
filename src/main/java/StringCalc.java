96b92ea01f12cf163a6d1e4991319e902c8396f7
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 22:00:15 +0530

    added test for 2 numbers

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -3,8 +3,13 @@ package src.main.java;
 public class StringCalc {
 
     public static int add(String s) {
-        if(!s.isEmpty())
-            return Integer.parseInt(s);
+        if(!s.isEmpty()){
+            String nums[] = s.split(",");
+            if(nums.length == 1)
+                return Integer.parseInt(nums[0]);
+            else
+                return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+        }
         return 0;
     }
 }
