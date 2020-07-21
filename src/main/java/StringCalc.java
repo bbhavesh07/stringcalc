71893a634078519d9bbded062c2b586d314faeec
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 22:02:49 +0530

    fixed code to sum n numbers also refactored the code

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -1,14 +1,12 @@
 package src.main.java;
 
+import java.util.Arrays;
+
 public class StringCalc {
 
     public static int add(String s) {
         if(!s.isEmpty()){
-            String nums[] = s.split(",");
-            if(nums.length == 1)
-                return Integer.parseInt(nums[0]);
-            else
-                return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+            return Arrays.stream(s.split(",")).map(x->Integer.parseInt(x)).reduce(0, (sum, x)-> sum+x);
         }
         return 0;
     }
