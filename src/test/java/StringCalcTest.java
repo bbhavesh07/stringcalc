ed1868dc6a834cfd5df9043bc28d9ea117f4fa8e
Bhavesh Bawadhane <bhavesh.bawadhane@niceactimize.com>
2020-07-21 21:52:16 +0530

    test failed for single number

--- src/test/java/StringCalcTest.java
+++ src/test/java/StringCalcTest.java
@@ -8,4 +8,9 @@ public class StringCalcTest {
     public void givenEmptyString_returns_zero(){
         assert(StringCalc.add("") == 0);
     }
+
+    @Test
+    public void givenNumber_returns_number(){
+        assert(StringCalc.add("1") == 1);
+    }
 }
